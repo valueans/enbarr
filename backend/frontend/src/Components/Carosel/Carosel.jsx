654c9ac5ba8ec0aa07f84c7edfff9f53e40f9dfa
shehroz kapoor <shehrozkapoor@gmail.com>
2023-02-04 01:00:31 +0500

    Fix horse details controlls not displayed

--- backend/frontend/src/Components/Carosel/Carosel.jsx
+++ backend/frontend/src/Components/Carosel/Carosel.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
+import {Card,CardMedia} from '@mui/material';
 
 const Carosel = ({images=[]}) => {
   return (
     <Carousel showArrows={true} swipeable={true} showThumbs={false} useKeyboardArrows={true} >
         {
             images.map((image)=>{
-                return <div key={image.id}>
-                  {
-                    image?.file_type === 'IMAGE'?
-                    <img alt="" src={image?.file} style={{height:"600px"}}/>:
-                    <video width="100%" height="100%" controls="controls autoplay">
-                        <source src={image?.file} type="video/mp4" />
-                    </video>
-                  }
-                    </div>
+                return <Card sx={{height:"500px",width:"100%"}} key={image.id}>
+                  <CardMedia
+                      component={image?.file_type === 'IMAGE'?'img':'video'}
+                      height="100%"
+                      image={image?.file}
+                      alt="add-image"
+                      autoPlay
+                      controls
+                    />
+                </Card>
             })
         }
     </Carousel>
