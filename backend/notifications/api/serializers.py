054803b7e48dd6d5258149c25eebdcdc9666551d
Shehroz Kapoor <59894229+shehrozkapoor@users.noreply.github.com>
2023-03-03 17:01:28 +0500

    Added user two profile in notifications (#159)
    * NotificationSerializer Update
    
    * Added user two profile in notifications

--- backend/notifications/api/serializers.py
+++ backend/notifications/api/serializers.py
@@ -1,10 +1,12 @@
 from notifications.models import Notifications
 from rest_framework import serializers
 from users.api.serializers import UserSerializer, UserProfileSerializer
+from chat.models import Conversation
 
 
 class NotificationsSerializer(serializers.ModelSerializer):
-    user_profile = serializers.SerializerMethodField("get_user_profile", read_only=True)
+    user_profile = serializers.SerializerMethodField(read_only=True)
+    user_two_profile = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Notifications
@@ -14,6 +16,7 @@ class NotificationsSerializer(serializers.ModelSerializer):
             "message_profile_url",
             "read_status",
             "user_profile",
+            "user_two_profile",
             "type",
             "channel_id",
             "horse_id"
@@ -24,3 +27,18 @@ class NotificationsSerializer(serializers.ModelSerializer):
         instance = obj.user.userprofile
         serializer = UserProfileSerializer(instance)
         return serializer.data
+
+    def get_user_two_profile(self,obj):
+        if obj.channel_id:
+            try:
+                conversations = Conversation.objects.get(channel=obj.channel_id)
+                if conversations.user_one.id != obj.user.id:
+                    instance = conversations.user_one
+                else:
+                    instance = conversations.user_two
+                serializer = UserProfileSerializer(instance)
+                return serializer.data
+            except:
+                return None
+        else:
+            return None
