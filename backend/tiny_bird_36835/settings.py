2a8942657d27c9bf4a060e78df2121c8230fc858
Shehroz Kapoor <shehrozkapoor@gmail.com>
2023-07-24 23:36:37 -0400

    remove local config

--- backend/tiny_bird_36835/settings.py
+++ backend/tiny_bird_36835/settings.py
@@ -409,6 +409,8 @@ GOOGLE_MAPS_API_KEY = env.str("GOOGLE_MAPS_API_KEY", "")
 # Mix Panel
 MIXPANEL_API_KEY = env.str("MIXPANEL_API_KEY", "")
 
-GDAL_LIBRARY_PATH = '/opt/homebrew/Cellar/gdal/3.6.4_6/lib/libgdal.32.dylib'
-GEOS_LIBRARY_PATH = '/opt/homebrew/Cellar/gdal/3.6.4_6/lib/libgdal.32.dylib'
+
+if env.str("GDAL_LIBRARY_PATH", None):
+    GDAL_LIBRARY_PATH = env.str("GDAL_LIBRARY_PATH", None)
+    GEOS_LIBRARY_PATH = env.str("GEOS_LIBRARY_PATH", None)
 
